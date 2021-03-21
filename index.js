67149e21d2e2191b7493804b019b74f55022538e
b3nj4m1n <b3nj4m1n@gmx.net>
2021-03-22 00:56:33 +0100

    Math rendering working

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
-katex.render("c = \\pm\\sqrt{a^2 + b^2}", document.getElementsByClass("question-latex-container")[0], {
-    throwOnError: false
-});
+window.onload = function() {
+    test_formula = "11 * 38";
+    katex.render(test_formula, document.getElementsByClassName('question-latex-container')[0], {
+        throwOnError: false
+    });
+    render_answer(document.getElementById('answer-input'));
+};
+
+function render_answer(caller) { 
+    latex = caller.value;
+    katex.render(latex, document.getElementsByClassName('answer-latex-container')[0], {
+        throwOnError: false
+    });
+}
+
+function toggle_search() {
+    box_container = document.getElementsByClassName("search-box-container")[0];
+    search_input = document.getElementById("search-input");
+    if (box_container.style.display == "none") {
+        box_container.style.display = "block";
+        search_input.focus();
+    }
+    else {
+        box_container.style.display = "none";
+    }
+}
+
+function search_envs() {
+}
+
+function skip_question() {
+    alert("Skipping question.");
+}
